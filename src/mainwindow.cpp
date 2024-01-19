73f27c27767d72f0aea48b1da5a0593402438f72
Simon Thorell <kontakt@simonthorell.se>
2024-01-19 21:26:07 +0100

    Add app logo to GUI

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPixmap> // Add Image
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Load the app logo image
+    QPixmap pix("assets/cat.png");
+
+    // Set the pixmap to the label
+    ui->label->setPixmap(pix);
+
+    // If you want to resize the label to fit the image
+    ui->label->setScaledContents(true);
+    ui->label->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
 }
 
 MainWindow::~MainWindow()
