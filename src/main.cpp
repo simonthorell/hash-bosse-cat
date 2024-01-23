e718161fd469fcde662172d656afbc98ecce2403
Simon Thorell <kontakt@simonthorell.se>
2024-01-23 10:38:42 +0100

    Retina display support

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "mainwindow.h"     // GUI
 
 int main(int argc, char *argv[]) {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // Enable High DPI Scaling
     QApplication app(argc, argv);
     MainWindow mainWindow;
     mainWindow.styleGUI(app);
